152fa4a41c44d3349467375cd883655478fcfc14
Danilo Herculano <didoherculano@gmail.com>
2021-10-07 15:07:24 -0300

    Cors adicionado

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,14 @@ const port = 3003
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
+const cors = require('./cors')
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(cors)
 
 server.listen(port, function (){
     console.log(`BACKEND is running on port ${port}`)
 })
+
+module.exports = server
